9df4abc469f87cda9ebd0136d850513e3d1e7181
Yura_viper <yuramcpecs@gmail.com>
2022-08-22 22:39:03 +0200

    Added conditional message render

--- src/components/Message/Message.tsx
+++ src/components/Message/Message.tsx
@@ -1,7 +1,11 @@
 import { useSelector } from "react-redux";
 
-function Message():JSX.Element{
+function Message():JSX.Element | null{
     const message = useSelector((state:any) => state.message);
+
+    if(!message.messageText){
+        return null;
+    }
 
     return(
         <div className="messageWindow">
