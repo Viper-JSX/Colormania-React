35ca5dfd606f8f3839a6ad50a4e14235553b1059
Yura_viper <yuramcpecs@gmail.com>
2022-08-23 20:41:44 +0200

    Added user variable to UserProfile and nickname is now rendering

--- src/components/User/User_profile.tsx
+++ src/components/User/User_profile.tsx
@@ -1,9 +1,14 @@
+import { useSelector } from "react-redux";
 import { Outlet } from "react-router";
+import UserClass from "../../classes/User";
 
 function UserProfile():JSX.Element{
+    const user: UserClass = useSelector((state:any) => state.user.user);
+    console.log(user)
+
     return(
         <div className="userProfile">
-            User Proile
+            { user.nickname } User Proile
             <Outlet />
         </div>
     );
