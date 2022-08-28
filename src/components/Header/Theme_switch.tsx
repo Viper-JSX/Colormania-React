81c643269a4bcb98d3ef96d972a5f002a397e506
Yura_viper <yuramcpecs@gmail.com>
2022-08-28 09:58:06 +0200

    configured the transition

--- src/components/Header/Theme_switch.tsx
+++ src/components/Header/Theme_switch.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 import { useSelector } from "react-redux";
 import { ThemeSwitchProps } from "../../typescript/types";
 import { themeConfig } from "../../various_things/app_config";
