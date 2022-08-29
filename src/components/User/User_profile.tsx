6fe60c879e7edf14207449822a781a194045dd10
Yura_viper <yuramcpecs@gmail.com>
2022-08-29 18:21:15 +0200

    Navlink replaced by GoToLinkButton component

--- src/components/User/User_profile.tsx
+++ src/components/User/User_profile.tsx
@@ -3,6 +3,7 @@ import { Outlet } from "react-router";
 import { NavLink } from "react-router-dom";
 import UserClass from "../../classes/User";
 import { UserProfileProps } from "../../typescript/types";
+import GoToLinkButton from "../General_reusable_components/Go_to_link_button";
 
 function UserProfile({ handleLogout } : UserProfileProps):JSX.Element{
     const user: UserClass = useSelector((state:any) => state.user.user);
@@ -16,8 +17,7 @@ function UserProfile({ handleLogout } : UserProfileProps):JSX.Element{
                 user.authorized ? 
                 <button className="logoutButton" onClick={handleLogout}>Logout</button>
                 :
-                <NavLink to="authorization/login"><button className="loginButton" onClick={handleLogout}>Login</button></NavLink>
-            }
+                <GoToLinkButton path="authorization/login">Login</GoToLinkButton>            }
             <Outlet />
         </div>
     );
