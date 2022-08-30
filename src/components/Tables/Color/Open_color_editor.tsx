6ceab7df5a597b52df056180fb245764547b3e70
Yura_viper <yuramcpecs@gmail.com>
2022-08-30 16:33:50 +0200

    Fixed some issues with Color sizing

--- src/components/Tables/Color/Open_color_editor.tsx
+++ src/components/Tables/Color/Open_color_editor.tsx
@@ -1,10 +1,13 @@
+import { useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
 import { stringToUrl } from "../../../api/string_to_url";
 import { OpenColorEditorProps } from "../../../typescript/types";
 
 function OpenColorEditor({ tableName, colorToEdit } : OpenColorEditorProps):JSX.Element{
+    const themeName = useSelector((state:any) => state.theme.themeName);
+    
     return(
-        <NavLink className={`openColorEditor`} to={`${stringToUrl(colorToEdit.oldColorName)}/edit`} state={ { tableName, colorToEdit }}>Edit</NavLink>
+        <NavLink className={`openColorEditor ${themeName}`} to={`${stringToUrl(colorToEdit.oldColorName)}/edit`} state={ { tableName, colorToEdit }}>Edit</NavLink>
     );
 }
 
