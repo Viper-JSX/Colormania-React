974d87fcf1e87941b38919025ef77d6a1f4886d9
Yura_viper <yuramcpecs@gmail.com>
2022-08-31 21:48:03 +0200

    Added Message window appear animation

--- src/components/Message/Message.tsx
+++ src/components/Message/Message.tsx
@@ -3,12 +3,12 @@ import { useSelector } from "react-redux";
 function Message():JSX.Element | null{
     const [message, themeName] = useSelector((state:any) => [state.message, state.theme.name]);
 
-    if(!message.messageText){
-        return null;
-    }
+    //if(!message.messageText){
+    //    return null;
+    //}
 
     return(
-        <div className={`messageWindow ${themeName}`}>
+        <div className={`messageWindow ${message.messageText ? "visible" : "hidden"} ${themeName} `}>
             <b className="messageText">{message.messageText}</b>
         </div>
     );
